d8027018891f7511bdc1dd4d2c1c8a9c37bdc34e
v3kmww <132175129+v3kmww@users.noreply.github.com>
2024-05-28 07:25:32 -0500

    d

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask
+import requests
 import os
 
 
 # Create a Flask application
 app = Flask(__name__)
+
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        if response.status_code == 200:
+            return response.json()['ip']
+        else:
+            return 'Unable to fetch public IP'
+    except Exception as e:
+        return f'Error: {e}'
+
 
 # Define a route for the root URL
 @app.route('/')
@@ -12,5 +24,6 @@ async def hello():
 
 # Run the Flask application
 if __name__ == '__main__':
-    print(os.system('nmcli dev show eth0'))
+    server_ip = get_public_ip()
+    print(f'Hello, World! The server public IP address is {server_ip}')
     app.run(debug=True)
