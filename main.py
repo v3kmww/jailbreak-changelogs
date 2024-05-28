2c904c9aeddb6724e2d01f0a637f196bb3f2d31f
v3kmww <132175129+v3kmww@users.noreply.github.com>
2024-05-28 07:47:42 -0500

    d

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-from flask import Flask
-import requests
-import os
-
+from flask import Flask, jsonify
 
 # Create a Flask application
 app = Flask(__name__)
 
-def get_public_ip():
-    try:
-        response = requests.get('https://api.ipify.org?format=json')
-        if response.status_code == 200:
-            return response.json()['ip']
-        else:
-            return 'Unable to fetch public IP'
-    except Exception as e:
-        return f'Error: {e}'
-
-
 # Define a route for the root URL
 @app.route('/')
 async def hello():
-    return 'Hello, World!'
-
+    with open('changelogs.txt', 'r') as f:
+        return f.read()
+    
 # Run the Flask application
 if __name__ == '__main__':
-    server_ip = get_public_ip()
-    print(f'Hello, World! The server public IP address is {server_ip}')
     app.run(debug=True)
